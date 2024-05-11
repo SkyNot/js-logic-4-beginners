c733a8950e6556626f89a216445b7e81436bdc55
SkyNot <igorrian.cntgm18@gmail.com>
2024-05-11 20:55:05 -0300

    Remover a função even_counter

--- src/5-even_counter.spec.ts
+++ src/5-even_counter.spec.ts
@@ -11,17 +11,7 @@
  */
 
 function evenCounter(numbers: number[]) {
-  if (numbers.length === 0) {
-    return undefined;
-  }
-
-  let count = 0;
-  for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] % 2 === 0){
-      count++;
-    }
-}
-return count;
+  // implemente sua lógica aqui
 }
 
 // Execute seus testes 👇
